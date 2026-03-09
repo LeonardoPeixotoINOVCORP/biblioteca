452f93ef99295292a1e5b6dd02df3f34dd50f7be
Leonardo Peixoto <leonardopeixoto2005@gmail.com>
2026-03-09 11:20:13 +0000

    Primeiro Commit

---
+++ resources/js/Pages/Autores/Index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Livros">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Autores
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <!-- conteúdo aqui -->
+            </div>
+        </div>
+    </AppLayout>
+</template>
